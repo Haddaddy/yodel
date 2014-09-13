cd29e17eb570920db8e90ebe23f77ad5354580bb
soren121 <soren121@sorenstudios.com>
2014-09-12 23:28:27 -0400

    ! Use WinJS classes

--- ykapi.js
+++ ykapi.js
@@ -3,16 +3,16 @@ function parse_time(timestr) {
     return moment(timestr, format).format("X");
 }
 
-function Location(latitude, longitude, delta) {
+var Location = WinJS.Class.define(function(latitude, longitude, delta) {
     this.latitude = latitude;
     this.longitude = longitude;
     if(delta == null) {
         delta = "0.030000";
     }
     this.delta = delta;
-}
+});
 
-function Comment(raw, message_id, client) {
+var Comment = WinJS.Class.define(function(raw, message_id, client) {
     this.client = client;
     this.message_id = message_id;
     this.comment_id = raw["commentID"];
@@ -21,33 +21,33 @@ function Comment(raw, message_id, client) {
     this.likes = parseInt(raw["numberOfLikes"]);
     this.poster_id = raw["posterID"];
     this.liked = parseInt(raw["liked"]);
-    
-    this.upvote = function() {
+    }, {
+    upvote: function() {
         if(this.liked == 0) {
             this.likes += 1;
             this.liked += 1;
             return this.client.upvote_comment(this.comment_id);
         }
-    }
-    this.downvote = function() {
+    },
+    downvote: function() {
         if(this.liked == 0) {
             this.likes -= 1;
             this.liked += 1;
             return this.client.downvote_comment(this.comment_id);
         }
-    }
-    this.report = function() {
+    },
+    report: function() {
         return this.client.report_comment(this.comment_id, this.message_id);
-    }
-    this.delete = function() {
+    },
+    delete: function() {
         if(this.poster_id == this.client.id) {
             return this.client.delete_comment(this.comment_id, this.message_id);
         }
-    }
-    this.reply = function(comment) {
+    },
+    reply: function(comment) {
         return this.client.post_comment(this.message_id, comment);
-    }
-    this.get_comment_pretty = function() {
+    },
+    get_comment_pretty: function() {
         var my_action = "";
         if(this.liked > 0) {
             my_action = "^";
@@ -57,9 +57,9 @@ function Comment(raw, message_id, client) {
         }
         return my_action + "(" + this.likes + ") " + this.comment;
     }
-}
+});
 
-function Yak(raw, client) {
+var Yak = WinJS.Class.define(function(raw, client) {
     this.client = client;
     this.poster_id = raw["posterID"];
     this.hide_pin = bool(int(raw["hidePin"]));
@@ -75,43 +75,43 @@ function Yak(raw, client) {
     this.type = raw["type"];
     this.liked = int(raw["liked"]);
     this.reyaked = raw["reyaked"];
-    
-    this.upvote = function() {
+    }, {
+    upvote: function() {
         if(this.liked == 0) {
             this.liked += 1;
             this.likes += 1;
             return this.client.upvote_yak(this.message_id);
         }
-    }
-    this.downvote = function() {
+    },
+    downvote: function() {
          if(this.liked == 0) {
             this.liked -= 1;
             this.likes += 1;
             return this.client.downvote_yak(this.message_id);
         }       
-    }
-    this.report = function() {
+    },
+    report: function() {
         return this.client.report_yak(this.message_id);
-    }
-    this.delete = function() {
+    },
+    delete: function() {
         if(this.poster_id == this.client.id) {
             return this.client.delete_yak(this.message_id);
         }
-    }
-    this.add_comment = function() {
+    },
+    add_comment: function() {
         return this.client.post_comment(this.message_id, comment);
-    }
-    this.get_comments = function() {
+    },
+    get_comments: function() {
         return this.client.get_comments(this.message_id);
-    }
-    this.get_yak_pretty = function() {
+    },
+    get_yak_pretty: function() {
         if(this.handle != null) {
             return this.message;
         }
     }
-}
+});
 
-function Yakker(user_id, loc, force_register) {
+var Yakker = WinJS.Class.define(function(user_id, loc, force_register) {
     this.base_url = "http://yikyakapp.com/api/";
     this.user_agent = "android-async-http/1.4.4 (http://loopj.com/android-async-http)";
 
@@ -131,11 +131,12 @@ function Yakker(user_id, loc, force_register) {
 
     this.id = user_id;
     this.handle = null;
-
-    this.gen_id = function() {
+    
+    }, {
+    gen_id: function() {
         CryptoJS.MD5(Math.floor(100000 + Math.random() * 900000)).toUppercase();
-    }
-    this.register_id_new = function(id) {
+    },
+    register_id_new: function(id) {
         params = {
             "userID": id,
             "lat": this.loc.latitude,
@@ -143,8 +144,8 @@ function Yakker(user_id, loc, force_register) {
         }
         result = this.get("registerUser", params);
         return result;
-    }
-    this.sign_request = function(page, params) {
+    },
+    sign_request: function(page, params) {
         var key = "35FD04E8-B7B1-45C4-9886-94A75F4A2BB4";
         /* Salt is current time (in sec) since epoch */
         var salt = String(Math.round(new Date().getTime() / 1000));
@@ -173,8 +174,8 @@ function Yakker(user_id, loc, force_register) {
         /* Add signature to request */
         params["hash"] = hash;
         params["salt"] = salt;
-    }
-    this.get = function(page, params) {
+    },
+    get: function(page, params) {
         url = this.base_url + page;
 
         this.sign_request(page, params);
@@ -192,34 +193,34 @@ function Yakker(user_id, loc, force_register) {
         
         var response = "";
         return response;    
-    }
-    this.get_yak_list = function(page, params) {
+    },
+    get_yak_list: function(page, params) {
         return this.parse_yaks(this.get(page, params));
-    }
-    this.parse_yaks = function(text) {
+    },
+    parse_yaks: function(text) {
         raw_yaks = text["messages"];
         yaks = [];
         for(var raw_yak in raw_yaks) {
             yaks.push(Yak(raw_yak, self));
         }
         return yaks;
-    }
-    this.parse_comments = function(text, message_id) {
+    },
+    parse_comments: function(text, message_id) {
         raw_comments = text["comments"];
         comments = [];
         for(var raw_comment in raw_comments) {
             comments.push(Comment(raw_comment, message_id, self));
         }
         return comments;
-    }
-    this.contact = function(message) {
+    },
+    contact: function(message) {
         params = {
             "userID": this.id,
             "message": message
         }
         return this.get("contactUs", params);
-    }
-    this.upvote_yak = function(message_id) {
+    },
+    upvote_yak: function(message_id) {
         params = {
             "userID": this.id,
             "messageID": message_id,
@@ -227,8 +228,8 @@ function Yakker(user_id, loc, force_register) {
             "long": this.loc.longitude
         }
         return this.get("likeMessage", params);
-    }
-    this.downvote_yak = function(message_id) {
+    },
+    downvote_yak: function(message_id) {
         params = {
             "userID": this.id,
             "messageID": message_id,
@@ -236,8 +237,8 @@ function Yakker(user_id, loc, force_register) {
             "long": this.loc.longitude
         }
         return this.get("downvoteMessage", params);
-    }
-    this.upvote_comment = function(comment_id) {
+    },
+    upvote_comment: function(comment_id) {
         params = {
             "userID": this.id,
             "commentID": comment_id,
@@ -245,8 +246,8 @@ function Yakker(user_id, loc, force_register) {
             "long": this.loc.longitude
         }
         return this.get("likeComment", params);
-    }
-    this.downvote_comment = function(comment_id) {
+    },
+    downvote_comment: function(comment_id) {
         params = {
             "userID": this.id,
             "commentID": comment_id,
@@ -254,8 +255,8 @@ function Yakker(user_id, loc, force_register) {
             "long": this.loc.longitude
         }
         return this.get("downvoteMessage", params);
-    }
-    this.report_yak = function(message_id) {
+    },
+    report_yak: function(message_id) {
         params = {
             "userID": this.id,
             "messageID": message_id,
@@ -263,8 +264,8 @@ function Yakker(user_id, loc, force_register) {
             "long": this.loc.longitude
         }
         return this.get("reportMessage", params);
-    }
-    this.delete_yak = function(message_id) {
+    },
+    delete_yak: function(message_id) {
         params = {
             "userID": this.id,
             "messageID": message_id,
@@ -272,8 +273,8 @@ function Yakker(user_id, loc, force_register) {
             "long": this.loc.longitude
         }
         return this.get("deleteMessage2", params);
-    }
-    this.report_comment = function(comment_id, message_id) {
+    },
+    report_comment: function(comment_id, message_id) {
         params = {
             "userID": this.id,
             "commentID": comment_id,
@@ -282,8 +283,8 @@ function Yakker(user_id, loc, force_register) {
             "long": this.loc.longitude
         }
         return this.get("reportMessage", params);
-    }
-    this.delete_comment = function(comment_id, message_id) {
+    },
+    delete_comment: function(comment_id, message_id) {
         params = {
             "userID": this.id,
             "commentID": comment_id,
@@ -292,59 +293,59 @@ function Yakker(user_id, loc, force_register) {
             "long": this.loc.longitude
         }
         return this.get("deleteComment", params);
-    }
-    this.get_greatest = function() {
+    },
+    get_greatest: function() {
         params = {
             "userID": this.id,
             "lat": this.loc.latitude,
             "long": this.loc.longitude
         }
         return this.get_yak_list("getGreatest", params);
-    }
-    this.get_my_tops = function() {
+    },
+    get_my_tops: function() {
         params = {
             "userID": this.id,
             "lat": this.loc.latitude,
             "long": this.loc.longitude
         }
         return this.get_yak_list("getMyTops", params);
-    }
-    this.get_recent_replied = function() {
+    },
+    get_recent_replied: function() {
         params = {
             "userID": this.id,
             "lat": this.loc.latitude,
             "long": this.loc.longitude
         }
         return this.get_yak_list("getMyRecentReplies", params);
-    }
-    this.update_location = function(loc) {
+    },
+    update_location: function(loc) {
         this.loc = loc;
-    }
-    this.get_my_recent_yaks = function() {
+    },
+    get_my_recent_yaks: function() {
         params = {
             "userID": this.id,
             "lat": this.loc.latitude,
             "long": this.loc.longitude
         }
         return this.get_yak_list("getMyRecentYaks", params);
-    }
-    this.get_area_tops = function() {
+    },
+    get_area_tops: function() {
         params = {
             "userID": this.id,
             "lat": this.loc.latitude,
             "long": this.loc.longitude
         }
         return this.get_yak_list("getAreaTops", params);
-    }
-    this.get_yaks = function() {
+    },
+    get_yaks: function() {
         params = {
             "userID": this.id,
             "lat": this.loc.latitude,
             "long": this.loc.longitude
         }
         return this.get_yak_list("getMessages", params);
-    }
-    this.post_yak = function(message, showloc, handle) {
+    },
+    post_yak: function(message, showloc, handle) {
         params = {
             "userID": this.id,
             "lat": this.loc.latitude,
@@ -358,8 +359,8 @@ function Yakker(user_id, loc, force_register) {
             params["hndl"] = this.handle;
         }
         return this.get("sendMessage", params);
-    }
-    this.get_comments = function(message_id) {
+    },
+    get_comments: function(message_id) {
         params = {
             "userID": this.id,
             "messageID": message_id,
@@ -367,8 +368,8 @@ function Yakker(user_id, loc, force_register) {
             "long": this.loc.longitude
         }
         return this.parse_comments(this.get("getComments", params), message_id);
-    }
-    this.post_comment = function(message_id, comment) {
+    },
+    post_comment: function(message_id, comment) {
         params = {
             "userID": this.id,
             "messageID": message_id,
@@ -377,8 +378,8 @@ function Yakker(user_id, loc, force_register) {
             "long": this.loc.longitude
         }
         return this.get("postComment", params);
-    }
-    this.peek = function(loc) {
+    },
+    peek: function(loc) {
         params = {
             "userID": this.id,
             "lat": loc.latitude,
@@ -387,4 +388,4 @@ function Yakker(user_id, loc, force_register) {
         }
         return this.get_yak_list("getPeekMessages", params);
     }
-}
+});
