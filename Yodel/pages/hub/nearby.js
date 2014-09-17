93d2a5f3da2111ce14c4638d404a96cfdca96675
Nicholas Narsing <soren121@sorenstudios.com>
2014-09-17 01:14:57 -0400

    Forgot to check if a yak has already been voted on. I am not a smart man.

--- Yodel/pages/hub/nearby.js
+++ Yodel/pages/hub/nearby.js
@@ -65,28 +65,29 @@
                             $(".section1page section").on("click", ".yak_down", yak_vote.bind({vote: "down"}));
                             function yak_vote(event) {
                                 var target = $(event.target);
-                                target.addClass("yak_voted");
-                                var vote_count_ele = target.siblings(".yak_votecount");
-                                var orig_vote_count = parseInt(vote_count_ele.text());
-                                var message_id = target.parents(".yak_container").data("mid");
-                                if(this.vote == "up") {
-                                    var promise = yakker.upvote_yak(message_id);
-                                    vote_count_ele.text(orig_vote_count + 1);
-                                }
-                                else if(this.vote == "down") {
-                                    var promise = yakker.downvote_yak(message_id);
-                                    vote_count_ele.text(orig_vote_count - 1);
-                                }
-                                else {
-                                    return;
-                                }
-                                promise.then(function (response) {
-                                    console.log(response);
-                                    if (!response.isSuccessStatusCode) {
-                                        target.removeClass("yak_voted");
-                                        vote_count_ele.text(orig_vote_count);
+                                if (!target.hasClass("yak_voted")) {
+                                    target.addClass("yak_voted");
+                                    var vote_count_ele = target.siblings(".yak_votecount");
+                                    var orig_vote_count = parseInt(vote_count_ele.text());
+                                    var message_id = target.parents(".yak_container").data("mid");
+                                    if (this.vote == "up") {
+                                        var promise = yakker.upvote_yak(message_id);
+                                        vote_count_ele.text(orig_vote_count + 1);
                                     }
-                                });
+                                    else if (this.vote == "down") {
+                                        var promise = yakker.downvote_yak(message_id);
+                                        vote_count_ele.text(orig_vote_count - 1);
+                                    }
+                                    if (typeof promise != undefined) {
+                                        promise.then(function (response) {
+                                            console.log(response);
+                                            if (!response.isSuccessStatusCode) {
+                                                target.removeClass("yak_voted");
+                                                vote_count_ele.text(orig_vote_count);
+                                            }
+                                        });
+                                    }
+                                }
                             }
                         });
                     });
