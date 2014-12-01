142e477d379219bb5ce68c9488bb7abe91222dc4
Nicholas Narsing <soren121@sorenstudios.com>
2014-12-01 01:47:12 -0500

    More concise fix for negative times

--- Yodel/js/ext_lib/moment-twitter.js
+++ Yodel/js/ext_lib/moment-twitter.js
@@ -43,50 +43,52 @@
 
     initialize = function (moment) {
         var twitterFormat;
-        twitterFormat = function (format, noPrefix) {
+        twitterFormat = function (format) {
             var diff, now, num, unit, unitStr;
             now = moment();
             diff = Math.abs(this.diff(now));
-            unit = null;
-            num = null;
-            if (diff <= second) {
-                unit = 'seconds';
-                num = 1;
-            } else if (diff < minute) {
-                unit = 'seconds';
-            } else if (diff < hour) {
-                unit = 'minutes';
-            } else if (diff < day) {
-                unit = 'hours';
-            } else if (format === 'short') {
-                if (diff < week) {
-                    unit = 'days';
+            unit = num = null;
+
+            if (now.diff(this) < 0) {
+                // If time is "in the future", immediately return 0 seconds ago
+                return '0s';
+            }
+            else {
+                if (diff <= second) {
+                    unit = 'seconds';
+                    num = 1;
+                } else if (diff < minute) {
+                    unit = 'seconds';
+                } else if (diff < hour) {
+                    unit = 'minutes';
+                } else if (diff < day) {
+                    unit = 'hours';
+                } else if (format === 'short') {
+                    if (diff < week) {
+                        unit = 'days';
+                    } else {
+                        noPrefix = true;
+                        return this.format('M/D/YY');
+                    }
                 } else {
                     noPrefix = true;
-                    return this.format('M/D/YY');
+                    return this.format('MMM D');
                 }
-            } else {
-                noPrefix = true;
-                return this.format('MMM D');
+                if (!(num && unit)) {
+                    num = moment.duration(diff)[unit]();
+                }
+                unitStr = unit = formats[unit][format];
+                if (format === 'long' && num > 1) {
+                    unitStr += 's';
+                }
+                return num + unitStr;
             }
-            if (!(num && unit)) {
-                num = moment.duration(diff)[unit]();
-            }
-            unitStr = unit = formats[unit][format];
-            if (format === 'long' && num > 1) {
-                unitStr += 's';
-            }
-            if (now.diff(this) < 0 && noPrefix !== true) {
-                num = 0;
-                unitStr = 's';
-            }
-            return num + unitStr;
         };
-        moment.fn.twitterLong = function (noPrefix) {
-            return twitterFormat.call(this, 'long', noPrefix);
+        moment.fn.twitterLong = function () {
+            return twitterFormat.call(this, 'long');
         };
-        moment.fn.twitter = moment.fn.twitterShort = function (noPrefix) {
-            return twitterFormat.call(this, 'short', noPrefix);
+        moment.fn.twitter = moment.fn.twitterShort = function () {
+            return twitterFormat.call(this, 'short');
         };
         return moment;
     };
